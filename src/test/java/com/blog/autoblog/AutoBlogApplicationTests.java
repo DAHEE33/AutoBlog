4d750a98619d6886e15d591742f1e24d210cab9b
Kimda <todayda1006@gmail.com>
2024-12-05 17:32:15 +0900

    Initial commit

---
+++ src/test/java/com/blog/autoblog/AutoBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.autoblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
